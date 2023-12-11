b9f4b8f1fe3000d27add7929cc0e63304fa7d530
3x!l <exil@n0t3x1l.ovh>
2023-12-11 11:34:21 +0000

    Handle errors

--- app/customers/create/page.tsx
+++ app/customers/create/page.tsx
@@ -22,30 +22,40 @@ export default function CreateCustomer() {
     data: FormData,
   ): Promise<formRes> => {
     'use server'
-    const name = data.get('name')
-    const address = data.get('address')
-    const phoneNumber = data.get('phone_number')
-    const validatedFields = schema.safeParse({
-      name: name,
-      address: address,
-      phoneNumber: phoneNumber,
-    })
+    try {
+      const name = data.get('name')
+      const address = data.get('address')
+      const phoneNumber = data.get('phone_number')
 
-    if (!validatedFields.success) {
-      return { message: 'Invalid user input', error: true }
-    }
+      const validatedFields = schema.safeParse({
+        name: name,
+        address: address,
+        phoneNumber: phoneNumber,
+      })
 
-    await prisma.customer.create({
-      data: {
-        name: validatedFields.data.name,
-        address: validatedFields.data.address,
-        phoneNumber: validatedFields.data.phoneNumber,
-      },
-    })
-    return {
-      message: 'Customer created',
-      error: false,
+      if (!validatedFields.success) {
+        throw new Error('Invalid user input')
+      }
+
+      await prisma.customer.create({
+        data: {
+          name: validatedFields.data.name,
+          address: validatedFields.data.address,
+          phoneNumber: validatedFields.data.phoneNumber,
+        },
+      })
+
+      return {
+        message: 'Customer created',
+        error: false,
+      }
+    } catch (error) {
+      return {
+        message: 'An error occurred while creating the customer',
+        error: true,
+      }
     }
   }
+
   return <Form createCustomer={createCustomer} />
 }
