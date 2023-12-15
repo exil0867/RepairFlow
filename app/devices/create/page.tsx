adb4e9c050c63d92ad26c21703ef275eea21e946
ربيع <rabyejlassi@gmail.com>
2023-12-15 17:13:16 +0000

    Add Devices page

--- app/devices/create/page.tsx
+++ app/devices/create/page.tsx
@@ -1,5 +1,61 @@
-import SignOut from '@/components/sign-out'
+import Image from 'next/image'
+import prisma from '@/lib/prisma'
+import Form from './form'
+import { hash } from 'bcrypt'
+import Link from 'next/link'
+import { z } from 'zod'
+
+const schema = z.object({
+  serialNumber: z.string(),
+  model: z.string(),
+  brand: z.string(),
+})
+
+export type formRes = {
+  message: string
+  error: boolean
+}
 
 export default function CreateDevice() {
-  return <></>
+  const createDevice = async (
+    prevState: any,
+    data: FormData,
+  ): Promise<formRes> => {
+    'use server'
+    try {
+      const serialNumber = data.get('serial_number')
+      const model = data.get('model')
+      const brand = data.get('brand')
+
+      const validatedFields = schema.safeParse({
+        serialNumber: serialNumber,
+        model: model,
+        brand: brand,
+      })
+
+      if (!validatedFields.success) {
+        throw new Error('Invalid user input')
+      }
+
+      await prisma.device.create({
+        data: {
+          serialNumber: validatedFields.data.serialNumber,
+          model: validatedFields.data.model,
+          brand: validatedFields.data.brand,
+        },
+      })
+
+      return {
+        message: 'Device created',
+        error: false,
+      }
+    } catch (error) {
+      return {
+        message: 'An error occurred while creating the device',
+        error: true,
+      }
+    }
+  }
+
+  return <Form createDevice={createDevice} />
 }
--- app/devices/page.tsx
+++ app/devices/page.tsx
@@ -1,5 +1,46 @@
-import SignOut from '@/components/sign-out'
+import prisma from '@/lib/prisma'
 
 export default function DisplayDevices() {
-  return <></>
+  const fetchDevices = async () => {
+    try {
+      const devices = await prisma.device.findMany({
+        include: {
+          applications: true,
+        },
+      })
+      return devices
+    } catch (error) {
+      console.error('Error fetching devices:', error)
+      return []
+    }
+  }
+
+  const renderDevices = async () => {
+    const devices = await fetchDevices()
+    return (
+      <div>
+        <h2>Devices</h2>
+        <ul>
+          {devices.map((device) => (
+            <li key={device.id}>
+              <p>Serial number: {device.serialNumber}</p>
+              <p>Model: {device.model}</p>
+              <p>Brand: {device.brand}</p>
+              {device.applications && (
+                <ul>
+                  {device.applications.map((app) => (
+                    <li key={app.id}>
+                      Application ID: {app.id}, Status: {app.status}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  return <>{renderDevices()}</>
 }
