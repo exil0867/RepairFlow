bed9ccb372089d4716dade983ebca2313d288fe1
3x!l <exil@n0t3x1l.ovh>
2024-01-05 12:45:02 +0000

    Update text

--- app/wizard/wizard.tsx
+++ app/wizard/wizard.tsx
@@ -149,7 +149,7 @@ export default function Component() {
         <div className='space-y-8'>
           <div>
             <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
-              Sign Up
+              Create an application
             </h2>
             <p className='mt-2 text-center text-sm text-gray-600'>
               Step {currentStep} of 3
