19eccfd9116e19ea2e34b69abc02ef7e8311e7a8
3x!l <exil@n0t3x1l.ovh>
2024-01-13 11:38:34 +0000

    Fix spacing issue

--- app/dashboard/wizard/wizard.tsx
+++ app/dashboard/wizard/wizard.tsx
@@ -146,27 +146,23 @@ export default function Component() {
         title='Edit application'
         footer={
           <>
-            <div className='flex items-center'>
-              {currentStep > 1 && (
-                <Button
-                  className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50'
-                  variant='outline'
-                  onClick={goToPreviousStep}
-                >
-                  Back
-                </Button>
-              )}
-            </div>
-            <div className='text-sm'>
-              {currentStep < 3 && (
-                <Button
-                  className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700'
-                  onClick={goToNextStep}
-                >
-                  Next
-                </Button>
-              )}
-            </div>
+            {currentStep > 1 && (
+              <Button
+                className='flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50'
+                variant='outline'
+                onClick={goToPreviousStep}
+              >
+                Back
+              </Button>
+            )}
+            {currentStep < 3 && (
+              <Button
+                className='flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700'
+                onClick={goToNextStep}
+              >
+                Next
+              </Button>
+            )}
             <p className='mt-2 text-center text-sm text-gray-600'>
               Step {currentStep} of 3
             </p>
