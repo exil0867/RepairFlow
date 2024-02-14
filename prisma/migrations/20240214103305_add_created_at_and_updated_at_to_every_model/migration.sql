c924594ecec069ec3f83f252fbd6f223457906c1
3x!l <exil@n0t3x1l.ovh>
2024-02-14 10:33:26 +0000

    Add `createdAt` and `updatedAt` to every model

---
+++ prisma/migrations/20240214103305_add_created_at_and_updated_at_to_every_model/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Application` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `ConcludedApplication` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Customer` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Device` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Application" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "ConcludedApplication" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Customer" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Device" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
